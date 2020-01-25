6ad13d03ab08de988aabd481c58089411d551ef6
Robert Greathouse <greathouse.robert@gmail.com>
2020-01-25 16:39:50 -0600

    Add InMemorySubscribertQueueEventBus

---
+++ eventsourcing-core/src/main/java/greenmoonsoftware/es/event/InMemorySubscriberQueueEventBus.java
@@ -0,0 +1,57 @@
+package greenmoonsoftware.es.event;
+
+import greenmoonsoftware.es.Bus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class InMemorySubscriberQueueEventBus implements Bus<Event, EventSubscriber> {
+    private final Map<EventSubscriber,BlockingQueue<Event>> subscriberToQueue = new HashMap<>();
+    private final Map<EventSubscriber,Thread> subscriberToThread = new HashMap<>();
+
+    @Override
+    public void post(Event payload) {
+        subscriberToQueue.values().forEach(x -> x.offer(payload));
+    }
+
+    @Override
+    public Bus<Event, EventSubscriber> register(EventSubscriber subscriber) {
+        LinkedBlockingQueue<Event> queue = new LinkedBlockingQueue<>();
+        subscriberToQueue.put(subscriber, queue);
+        Thread t = new Thread(new EventSubscriberRunnable(subscriber, queue));
+        t.start();
+        subscriberToThread.put(subscriber, t);
+        return this;
+    }
+
+    @Override
+    public void unregister(EventSubscriber subscriber) {
+        subscriberToQueue.remove(subscriber);
+        subscriberToThread.remove(subscriber);
+    }
+
+    private static class EventSubscriberRunnable implements Runnable {
+        private final String label;
+        private final EventSubscriber subscriber;
+        private final BlockingQueue<Event> queue;
+        private volatile boolean keepRunning = true;
+        private EventSubscriberRunnable(EventSubscriber subscriber, BlockingQueue<Event> queue) {
+            this.label = subscriber.getClass().getSimpleName();
+            this.subscriber = subscriber;
+            this.queue = queue;
+        }
+        @Override
+        public void run() {
+            while(keepRunning) {
+                try {
+                    subscriber.onEvent(queue.take());
+                }  catch (Throwable t) {
+                    System.out.println(label + ": Caught Throwable");
+                    t.printStackTrace();
+                }
+            }
+        }
+    }
+}
